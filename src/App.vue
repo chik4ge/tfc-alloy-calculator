db313f9b6edbd552dcdc7856f8475b6cd3dbaae1
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 00:28:44 +0900

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import NabBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <div class="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-5xl">
+    <NabBar />
+  </div>
 </template>
 
 <style scoped></style>
