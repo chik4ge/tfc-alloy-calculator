282a4626080b65bf2dda648094f4c818c754b1b7
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 00:03:33 +0900

    install daisyUI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,35 +7,28 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <button class="btn btn-primary" @click="count++">
+    count is: {{ count }}
+  </button>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <!--- サイズのバリエーション --->
+  <button class="btn btn-lg">Large</button>
+  <button class="btn">Normal</button>
+  <button class="btn btn-sm">Small</button>
+  <button class="btn btn-xs">Tiny</button>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <!--- カラーバリエーション --->
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+  <button class="btn btn-link">Link</button>
+
+  <!--- その他 --->
+  <button class="btn btn-outline">Outline</button>
+  <button class="btn glass">Glass button</button>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
