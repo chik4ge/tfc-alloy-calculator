d159d20230f2dfde1256b036ee7e311f028899e5
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 03:53:43 +0900

    setup prettier

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import NabBar from './components/NavBar.vue'
+import Inventory from "./components/Inventory.vue"
+import NabBar from "./components/NavBar.vue"
 </script>
 
 <template>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
   <div class="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-5xl">
     <NabBar />
+
+    <Inventory />
   </div>
 </template>
 
---
+++ src/types/metal.ts
@@ -0,0 +1,7 @@
+enum Metal {
+  Copper = 0,
+  Gold = 1,
+  Silver = 2,
+}
+
+export default Metal
