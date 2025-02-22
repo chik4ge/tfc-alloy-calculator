b279d299b09111ac67e20b2fb10dda0941caa282
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 21:55:50 +0900

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,57 @@
 import Inventory from "./components/Inventory.vue"
 import NavBar from "./components/NavBar.vue"
 import TargetSelector from "./components/TargetSelector.vue"
+import CalculateButton from "./components/CalculateButton.vue"
+import CalculateResult from "./components/CalculateResult.vue"
+import Footer from "./components/Footer.vue"
+import { ref, type Ref } from "vue"
+import type { Item } from "./types/item"
+import type { Metal } from "./types/metal"
+import type { Result } from "./types/result"
+
+const inventory: Ref<Item[]> = ref([
+  {
+    metal: "ZINC",
+    grade: "NORMAL",
+    amount: 10,
+  },
+  {
+    metal: "COPPER",
+    grade: "NORMAL",
+    amount: 10,
+  },
+  {
+    metal: "TIN",
+    grade: "NORMAL",
+    amount: 10,
+  },
+])
+
+const target: Ref<Metal> = ref("BISMUTH_BRONZE")
+const targetAmount: Ref<number> = ref(100)
+
+const result: Ref<Result> = ref({ recipes: [] })
+
+const calculate = () => {
+  console.log("Calculate")
+}
 </script>
 
 <template>
   <div class="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-5xl">
     <NavBar />
 
-    <Inventory />
+    <Inventory :inventory="inventory" />
 
-    <TargetSelector />
+    <TargetSelector :targetAlloy="target" :targetAmount="targetAmount" />
+
+    <CalculateButton :clicked="calculate" />
 
     <div class="divider" />
+
+    <CalculateResult />
   </div>
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons"
+</script>
+
+<template>
+  <footer class="footer footer-center fixed flex flex-col bottom-0 bg-base-200 text-base-content rounded p-10">
+    <nav class="grid grid-flow-col gap-6">
+      @ 2025 chik4ge
+      <a class="link link-hover flex gap-2 items-center" href="https://github.com/chik4ge/tfc-alloy-calculator">
+        <FontAwesomeIcon :icon="faGithub" size="xl" />
+        Source
+      </a>
+      <a class="link link-hover flex gap-2 items-center" href="https://x.com/chik4ge">
+        <FontAwesomeIcon :icon="faTwitter" size="xl" />
+        Twitter
+      </a>
+    </nav>
+  </footer>
+</template>
