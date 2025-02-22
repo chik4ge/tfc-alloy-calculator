4db402de177b327bd2210620b47ff812a2667374
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 20:28:10 +0900

    fix error / add TargetSelector

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import Inventory from "./components/Inventory.vue"
-import NabBar from "./components/NavBar.vue"
+import NavBar from "./components/NavBar.vue"
+import TargetSelector from "./components/TargetSelector.vue"
 </script>
 
 <template>
   <div class="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-5xl">
-    <NabBar />
+    <NavBar />
 
     <Inventory />
+
+    <TargetSelector />
+
+    <div class="divider" />
   </div>
 </template>
 
---
+++ src/components/TargetSelector.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Recipe } from "../types/recipe"
+import { ref } from "vue"
+import { snakeToCamelWithSpaces } from "../common/stringUtils"
+
+const metalsWithRecipes = ref(Object.keys(Recipe))
+</script>
+
+<template>
+  <div class="overflow-x-auto grid gap-2">
+    <h2 class="text-2xl">Target</h2>
+    <div class="flex gap-2">
+      <label class="form-control w-full max-w-xs">
+        <select class="select select-bordered">
+          <option selected disabled>Target Alloy</option>
+          <option v-for="metal in metalsWithRecipes" :key="metal">{{ snakeToCamelWithSpaces(metal) }}</option>
+        </select>
+      </label>
+
+      <input
+        type="number"
+        placeholder="Target Amount (for Ingot: 100mB)"
+        class="input input-bordered w-full max-w-xs"
+      />
+    </div>
+  </div>
+</template>
