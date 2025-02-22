b6ad6fcd23870b69c1afd4553286bdfd9d1936a5
chikage <35529908+chik4ge@users.noreply.github.com>
2025-02-22 22:56:14 +0900

    add label to target amount

--- src/components/TargetSelector.vue
+++ src/components/TargetSelector.vue
@@ -17,11 +17,14 @@ const metalsWithRecipes = ref(Object.keys(Recipe))
         </select>
       </label>
 
-      <input
-        type="number"
-        placeholder="Target Amount (for Ingot: 100mB)"
-        class="input input-bordered w-full max-w-xs"
-      />
+      <label class="input">
+        <input
+          type="number"
+          placeholder="Amount (for Ingot: 100mB)"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <span class="label">mB</span>
+      </label>
     </div>
   </div>
 </template>
